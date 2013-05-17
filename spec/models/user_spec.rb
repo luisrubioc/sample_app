cf5f87119177c1a98a279c32a2f02ac089ef0032
Luis Rubio <fish834@gmail.com>
2013-05-17 21:06:49 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
 
@@ -124,7 +125,10 @@ describe User do
     end
   end
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank } # equivalent to: it { @user.remember_token.should_not be_blank }
+  end
 
 end
 
